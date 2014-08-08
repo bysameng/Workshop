b754af5b38dc561281ea35c3e726e36de526ecbf
Samuel Eng <sam.eng@icloud.com>
2014-08-08 12:33:13 -0400

    Update PlaneTilter.cs

--- Assets/Scripts/PlaneTilter.cs
+++ Assets/Scripts/PlaneTilter.cs
@@ -5,27 +5,28 @@ public class PlaneTilter : MonoBehaviour {
 
 	private bool rotationDirection; // if true, rotate counterclockwise
 
-	// Use this for initialization
-	void Start () {
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		Quaternion delta;
 
-
 		if (rotationDirection){
+			//rotate counterclockwise
 			delta = Quaternion.Euler(new Vector3(0, 0, 10f) * Time.deltaTime);
 		}
-		else{
+		
+		else {
+			//rotate clockwise
 			delta = Quaternion.Euler(new Vector3(0, 0, -10f) * Time.deltaTime);
 		}
 
+		//apply the rotation with our rigidbody
 		rigidbody.MoveRotation(rigidbody.rotation * delta);
 
 		//so here's where we need more logic
 		//since the rotations wrap around to 360, we need some maths!
 		if (rigidbody.rotation.eulerAngles.z < 350 && rigidbody.rotation.eulerAngles.z > 10){
+			//switch our rotation direction
 			rotationDirection = true;
 		}
 		if (rigidbody.rotation.eulerAngles.z > 10 && rigidbody.rotation.eulerAngles.z < 340){
